1cf14e4ca124ab2ba50520cd6ca3e4a8c17bb8b3
vaibhavkollipara <vaibhav.kollipara@gmail.com>
2017-10-07 14:05:51 -0400

    Layout setup complete.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import './App.css';
+import Display from './components/Display';
+import Input from './components/Input';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Display />
+      <Input />
       </div>
     );
   }
---
+++ src/components/Input/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+
+    render(){
+        return(
+            <div className="Input">
+            </div>
+        );
+    }
+}
+
+export default Input;
